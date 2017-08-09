5c26249df2b6e93b5c3a32e08fda78a1cfd65b26
Mitchell van Wijngaarden <mitchell@kooding.nl>
2017-08-09 15:48:59 +0200

    change error handling

---
+++ src/ErrorHandlingBootstrap.php
@@ -0,0 +1,15 @@
+<?php namespace Monolith\ErrorHandling;
+
+use Monolith\ComponentLoading\ComponentBootstrap;
+use Monolith\DependencyInjection\Container;
+
+class ErrorHandlingBootstrap implements ComponentBootstrap {
+    public function bind(Container $container): void {
+        $container->bind(ScreenDumpHandler::class);
+    }
+
+    public function init(Container $container): void {
+        error_reporting(E_ALL);
+        set_exception_handler([$container[ScreenDumpHandler::class], 'handle']);
+    }
+}
