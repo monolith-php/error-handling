85bd2b44701d9509adba616098d68ae452ed8321
Shawn McCool <shawn@heybigname.com>
2018-11-01 14:17:01 +0100

    pokayoke

--- src/ErrorHandlingBootstrap.php
+++ src/ErrorHandlingBootstrap.php
@@ -7,7 +7,7 @@ final class ErrorHandlingBootstrap implements ComponentBootstrap
 {
     public function bind(Container $container): void
     {
-        if (getenv('SHOW_DEBUG_ERRORS')) {
+        if (getenv('SHOW_DEBUG_ERRORS') == 'on') {
             $whoops = new \Whoops\Run;
             $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
             $whoops->register();
